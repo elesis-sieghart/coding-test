2284ad3753ddbf98235726ce243cd6015aded4ea
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-06 12:47:01 +0900

    Update 동명 동물 수 찾기.sql

--- 프로그래머스/lv2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
+++ 프로그래머스/lv2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -5,3 +5,7 @@ FROM (SELECT NAME, COUNT(*) AS COUNT
       GROUP BY NAME
       ORDER BY NAME) A
 WHERE A.COUNT>=2 AND A.NAME IS NOT NULL;
+
+-- 의문점: GROUP BY는 NULL 집계가 안된다고 배워서 IS NOT NULL 조건을 빼고 풀었는데, 그때는 오답 처리되었다. 왜그럴까?
+-- 
+-- https://school.programmers.co.kr/questions/28525
