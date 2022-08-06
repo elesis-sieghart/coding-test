47df7fd1c09efbb89f4805c64df4938cee4f6a31
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-06 16:15:15 +0900

    Update 입양 시각 구하기（2）.sql

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,7 @@
 WITH RECURSIVE test AS ( 
-    SELECT 0 AS num # Non-Recursive 문장( 첫번째 루프에서만 실행됨 )
+    SELECT 0 AS num                                  # Non-Recursive 문장( 첫번째 루프에서만 실행됨 )
     UNION ALL
-    SELECT num+1 AS num FROM test WHERE num+1<24 # Recursive 문장(읽어 올 때마다 행의 위치가 기억되어 다음번 읽어 올 때 다음 행으로 이동함)
+    SELECT num+1 AS num FROM test WHERE num+1<24     # Recursive 문장(읽어 올 때마다 행의 위치가 기억되어 다음번 읽어 올 때 다음 행으로 이동함)
 )
 SELECT T.num, COALESCE(H.COUNT, 0) FROM TEST T
 LEFT JOIN (
