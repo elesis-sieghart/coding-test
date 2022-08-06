669e4af2777ab2e7de10959fc4805f254df50460
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-06 11:38:35 +0900

    Update 상위 n개 레코드.sql

--- 프로그래머스/lv1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/lv1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,2 +1,12 @@
--- 코드를 입력하세요
+-- ORDER BY, LIMIT
 SELECT NAME FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1;
+
+-- SUBQUERY
+SELECT NAME
+from ANIMAL_INS
+where DATETIME=(SELECT MIN(DATETIME)
+                from ANIMAL_INS);
+                
+-- 최악의 경우(index가 없는 경우) min 이 order by limit 1 보다 빠르다. 즉, min 을 사용하는 것이 좋다.
+-- 최선의 경우(index가 있는 경우) min 과 order by limit 1 의 성능이 비등비등하다. 
+-- https://stackoverflow.com/a/426785
