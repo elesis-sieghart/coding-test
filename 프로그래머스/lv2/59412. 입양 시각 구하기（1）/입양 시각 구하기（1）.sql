d87941203c7e34ac17cc2c7105ab1d304b6bf465
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-06 13:53:32 +0900

    Update 입양 시각 구하기（1）.sql

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -7,8 +7,7 @@ ORDER BY HOUR(DATETIME)
 */
 
 -- HAVING HOUR(DATETIME)을 거니까 SELECT HOUR(DATETIME)시 에러가 발생했다.
--- HAVING 이후 조건을 거는 column은 
--- 추가적인 COMMAND가 가미되지 않은 아닌 순수한 column명을 그대로 써줘야한다.
+-- HAVING 이후 조건을 거는 column은 추가적인 COMMAND가 가미되지 않은 아닌 순수한 column명을 그대로 써줘야한다.
 -- https://medium.com/learn-from-data/having%EC%9D%98-%EB%B9%84%EB%B0%80-7ebf56834436
 
 -- 제출 쿼리 (에러났던 쿼리를 돌아가게 하려고 SUBQUERY를 사용함)
