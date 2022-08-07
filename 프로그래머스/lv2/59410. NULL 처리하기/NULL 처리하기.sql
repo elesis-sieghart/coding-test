a57456bf08d6e46850885547353889420da00e09
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-08 04:11:58 +0900

    Update NULL 처리하기.sql

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -7,12 +7,11 @@ ORDER BY ANIMAL_ID
 COALESCE 함수는 여러개의 파라미터, IFNULL은 두개의 파라미터
 COALESCE 함수는 표준 SQL 함수, IFNULL은 MYSQL에 있는 함수
 더 유연하고 표준함인 것은 COALESCE 함수 
+https://velog.io/@gillog/DB-MySQL-NULL-%EC%B2%98%EB%A6%ACIFNULL-CASE-COALESCE
 */
--- https://velog.io/@gillog/DB-MySQL-NULL-%EC%B2%98%EB%A6%ACIFNULL-CASE-COALESCE
 
 -- NVL, NVL2
 /*
 IFNULL이 MySQL함수 였다면 NVL, NVL2는 Oracle함수
+https://cheershennah.tistory.com/211
 */
--- https://cheershennah.tistory.com/211
-
