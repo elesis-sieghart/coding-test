8bbc5de571d0b55e892522ad3cff01d58dbb654c
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-08 07:27:05 +0900

    Update DATETIME에서 DATE로 형 변환.sql

--- 프로그래머스/lv2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/lv2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,4 +1,5 @@
--- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME,'%Y-%m-%d') AS '날짜'
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
+
+-- https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html
