2efba6cf87b19aa0c0e23192c2d5dfe6b9dad88a
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-08 06:29:08 +0900

    Update 루시와 엘라 찾기.sql

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,8 @@
--- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE 
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
 ORDER BY ANIMAL_ID
+
+-- LIKE를 사용해서 찾는법
+WHERE 'Lucy, Ella, Pickle, Rogan, Sabrina, Mitty' LIKE CONCAT('%', NAME, '%')
+-- https://school.programmers.co.kr/questions/25496
