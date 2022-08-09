18becf41fe3079c3db60feeef51b1fa49034e073
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-10 03:47:31 +0900

    Update 없는 숫자 더하기.py

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -3,3 +3,11 @@ def solution(numbers):
     for i in numbers:
         val = val - i
     return val
+
+
+# sum
+def solution(numbers):
+    return 45 - sum(numbers)
+
+# lambda
+solution = lambda x: sum(range(10)) - sum(x)
