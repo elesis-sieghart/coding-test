1777745ce0579c5fd408e2786295d15b4b331391
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-11 04:14:43 +0900

    Update 사파리월드.py

--- 백준/Bronze/2420. 사파리월드/사파리월드.py
+++ 백준/Bronze/2420. 사파리월드/사파리월드.py
@@ -1,2 +1,18 @@
 a, b = map(int, input().split())
 print(abs(a-b))
+
+# 시간 0ms: Fortran(1954), Pascal(1969), Lua(1993), C99(1999), C11(99후속, 2011), C++17(2018), Rust(2010), Kotlin(2011)
+# 메모리: 저 중에서도 압도적인 메모리 Pascal(336KB)
+# 메모리: 그외에는 C와 C++들(1112KB), Clang들(1124KB), Fortran(1408KB), Lua(9764KB), Rust들(13024KB), Kotlin(14256KB)
+# 파이썬은 제일 빠른게 29056KB, 52ms 이었다.
+
+'''
+C99
+C언어의 표준은 ANSI라는 이름의 미국 표준 협회에서 제정하고, 이렇게 ANSI에서 제정한 C언어의 1999년도 최신 표준안을 가리켜 C99라고 한다.
+
+Pascal, Delphi
+Pascal 출시 -> Pascal로 개발할 수 있는 델파이 IDE 출시(1990년대) -> 영향력이 강해진 IDE 개발사
+-> Pascal의 업그레이드 버전인 Object Pascal과는 별도로 IDE 개발사에서 Pascal에 독자적 기능을 업데이트(Object Pascal이라고 부르기 어려움) 
+-> 시간이 흐르며 Object Pascal과 구분하기위해 Delphi언어라 부르게됨 -> 결론은 Delphi는 언어이자 IDE 입니다.
+https://tech.devgear.co.kr/delphi_qna/451028
+'''
