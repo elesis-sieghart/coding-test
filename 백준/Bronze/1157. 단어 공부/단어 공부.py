e9b2758bb1002a7c249d486e5a3d73252b2cabea
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-12 04:56:08 +0900

    Update 단어 공부.py

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -11,3 +11,22 @@ if(lst.count(max(lst))>1):
 else:
     i = lst.index(max(lst))
     print(chr(i+65))
+    
+# 다른 풀이 (32796KB, 88ms)
+s = input().upper();
+c = [s.count(chr(i)) for i in range(65, 91)]
+m = max(c)
+if(c.count(m) == 1):
+    print(chr(c.index(m)+65))
+else:
+    print('?')
+    
+'''
+c = [s.count(chr(i)) for i in range(65, 91)]
+
+위 코드와 아래 코드의 결과는 같다.
+
+c = []
+for i in range(65, 91):
+    c.append(s.count(chr(i)))
+'''
