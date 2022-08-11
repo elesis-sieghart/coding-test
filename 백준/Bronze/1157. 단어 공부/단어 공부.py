965b9c319498df2dd6f44eaeac14e009fb62a242
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-12 04:59:01 +0900

    Update 단어 공부.py

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -24,7 +24,8 @@ else:
 '''
 c = [s.count(chr(i)) for i in range(65, 91)]
 
-위 코드와 아래 코드의 결과는 같다.
+위 코드(리스트 내포)와 아래 코드의 결과는 같다. 
+https://wikidocs.net/22#for-range_1:~:text=%3E%3E%3E%20result%20%3D%20%5Bnum%20*%203%20for%20num%20in%20a%5D
 
 c = []
 for i in range(65, 91):
