bb1f9190aff9636edfb9120206707acdaae51202
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-13 13:48:33 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,14 +1,13 @@
-import sys
+n = int(input())
+words = []
+for _ in range(n):
+    words.append(input())
 
-num = int(input())
-arr = [sys.stdin.readline().strip() for _ in range(num)]
-
-origin = arr[0]
-wordLen = len(origin)
-
-for j in range(num):
-    tmp = ''
-    for i in range(wordLen):
-        tmp += (lambda x, y: x if x==y else '?')(origin[i], arr[j][i])
-    origin = tmp
-print(origin)
+m = len(words[0])
+answer = list(words[0])
+for i in range(m):
+    for j in range(1, n):
+        if answer[i] != words[j][i]:
+            answer[i] = '?'
+    
+print(''.join(answer))
