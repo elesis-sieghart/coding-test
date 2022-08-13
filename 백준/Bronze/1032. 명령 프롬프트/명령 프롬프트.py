c22193f779bfc7970be2bb545495a18af9cdb649
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-13 13:50:04 +0900

    Update 명령 프롬프트.py

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,3 +1,19 @@
+import sys
+
+num = int(input())
+arr = [sys.stdin.readline().strip() for _ in range(num)]
+
+origin = arr[0]
+wordLen = len(origin)
+
+for j in range(num):
+    tmp = ''
+    for i in range(wordLen):
+        tmp += (lambda x, y: x if x==y else '?')(origin[i], arr[j][i])
+    origin = tmp
+print(origin)
+
+# 다른 풀이 (동일)
 n = int(input())
 words = []
 for _ in range(n):
