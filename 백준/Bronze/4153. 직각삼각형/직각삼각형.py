9dbfda4ee3ccf2c2cbedb304af5da6f29b8621f0
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-13 15:20:45 +0900

    Update 직각삼각형.py

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,12 +1,36 @@
-while True:
-    num = list(map(int,input().split()))
-    max_num = max(num)
+import sys
 
-    if max_num ==0:
-        break
+while True:
+    val = list(map(int, sys.stdin.readline().strip().split()))
+
+    if len(val)==3 and val[0]==0 and val[1]==0 and val[2]==0: break
+    maxNum = max(val)
+    val.remove(maxNum)
+    print('right') if val[0]**2 + val[1]**2 == maxNum**2 else print('wrong')
     
-    num.remove(max_num)
-    if max_num**2==num[0]**2+num[1]**2:
-            print("right")
+# 다른 풀이
+while True :
+    nums = list(map(int, input().split()))
+    
+    if sum(nums) == 0:
+        break  # 세 수가 0이면 break
+        
+    max_num = max(nums)
+    nums.remove(max_num)  # 빗변의 길이는 직각삼각형 세변의 길이중 가장 길다.
+    if nums[0]**2 + nums[1]**2 == max_num**2:
+        print('right')
     else:
-        print("wrong")
+        print('wrong')
+        
+# del, remove
+'''
+del 예약어 : 인덱스로 삭제
+remove( ) 함수 : 값으로 삭제 (첫번째 값만 삭제)
+
+del arr[인덱스]
+del arr[3:]
+
+arr.remove('a')
+'''
+
+# 파이썬 예약어 종류: del, if, for, or, and
