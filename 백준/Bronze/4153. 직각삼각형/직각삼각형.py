706598fcdb80d91ef324002d87c22c31c037d804
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-13 15:14:48 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,10 +1,12 @@
-import sys
-
 while True:
-    val = list(map(int, sys.stdin.readline().strip().split()))
+    num = list(map(int,input().split()))
+    max_num = max(num)
 
-    if len(val)==3 and val[0]==0 and val[1]==0 and val[2]==0:
+    if max_num ==0:
         break
-    maxNum = max(val)
-    val.remove(maxNum)
-    print('right') if val[0]**2 + val[1]**2 == maxNum**2 else print('wrong')
+    
+    num.remove(max_num)
+    if max_num**2==num[0]**2+num[1]**2:
+            print("right")
+    else:
+        print("wrong")
