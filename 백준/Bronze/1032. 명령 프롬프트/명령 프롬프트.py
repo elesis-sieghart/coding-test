05a74a9bd3b2d21c92d9287c60f88bcf734abd7b
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-13 13:47:52 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,17 +1,14 @@
 import sys
 
-valOrQuestion = lambda x, y: x if x==y else '?'
-
 num = int(input())
+arr = [sys.stdin.readline().strip() for _ in range(num)]
 
-origin = sys.stdin.readline().strip()
+origin = arr[0]
 wordLen = len(origin)
-# 파일 이름의 길이는 모두 같다.
-arr = [sys.stdin.readline().strip() for _ in range(num-1)]
 
-for j in range(num-1):
+for j in range(num):
     tmp = ''
     for i in range(wordLen):
-        tmp += valOrQuestion(origin[i], arr[j][i])
-    origin=tmp
+        tmp += (lambda x, y: x if x==y else '?')(origin[i], arr[j][i])
+    origin = tmp
 print(origin)
