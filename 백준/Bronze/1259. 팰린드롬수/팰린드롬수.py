0234327c4203ce722922bd34161509e68814d358
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-14 19:20:54 +0900

    Update 팰린드롬수.py

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -15,3 +15,14 @@ while True:
 
     if -1 in result: print('no')
     else: print('yes')
+
+# 다른 풀이 (29284KB 52ms)
+import sys
+
+input=sys.stdin.readline
+
+while 1:
+  s=input().rstrip()
+  if s=='0': break
+  print('yes' if s==s[::-1] else 'no')
+# 그냥! 뒤집어서 같으면 되는건데! 
