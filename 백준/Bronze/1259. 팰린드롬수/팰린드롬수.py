cbffd377a66b18b7a99a06a622dcea5a69a99411
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-14 19:12:45 +0900

    [Bronze I] Title: 팰린드롬수, Time: 76 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,15 @@
+import math
+
+def check(val, i):
+    if val[i]==val[-(i+1)]:
+        return 'ok'
+    else:
+        return -1
+
+while True:
+    val = list(map(int, input()))
+    if len(val)==1 and val[0]==0 : break
+    result = [check(val, i) for i in range(math.floor(len(val)/2))]
+
+    if -1 in result: print('no')
+    else: print('yes')
