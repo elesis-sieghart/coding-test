a9772a1a88b210ae1327ff5d72ade1d3086401fa
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-16 18:58:45 +0900

    Update 신고 결과 받기.py

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,3 +1,30 @@
+# 3 9 10 11 14 15 20 21 시간 초과
+def solution(id_list, report, k):
+    # 중복 신고 제거
+    check_report = set(report)
+        
+    # 특정 유저 중 리포트 받은 사람의 리포팅 수 카운트
+    result = [0] * len(id_list)
+    
+    for index, user in enumerate(id_list):
+        for reporting in check_report:
+            if(user == reporting.split()[1]): result[index] += 1
+    
+    # 리포팅이 k 이상인 사람 색출
+    re = [id_list[index] if value>=k else 0 for index, value in enumerate(result)]
+
+    # 신고자가 받는 메시지 카운트
+    answer = [0] * len(id_list)
+    reportCnt = [0] * len(id_list)
+    for index, bannedUser in enumerate(re):
+        for reporting in check_report:
+            if(bannedUser == reporting.split()[1]): 
+                answer[id_list.index(reporting.split()[0])] += 1
+            
+    return answer
+
+
+# 다른 풀이
 def solution(id_list, report, k):
     s1 = [s.split()[1] for s in set(report)]
     s2 = [s for s in set(s1) if s1.count(s) >= k]
