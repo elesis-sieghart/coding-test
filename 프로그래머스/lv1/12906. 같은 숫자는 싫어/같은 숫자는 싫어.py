d1fdd50fb2b2d9f5d27871fab2c338d8a1ab878a
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-16 22:56:58 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,3 +1,17 @@
+# 작성 코드 (95MB, 400ms)
+def solution(arr):
+    arr = list(map(str, arr))
+
+    result = ''
+    for i in range(len(arr)):
+        if i==0: 
+            result += arr[i]
+        else: 
+            result += ((lambda x, y: y if x!=y else '')(arr[i-1], arr[i]))
+
+    return list(map(int, result)) 
+
+# 다른 풀이 (29MB, 40ms)
 def solution(arr):
     answer = []
     j = -1
@@ -7,3 +21,5 @@ def solution(arr):
             answer.append(i)
 
     return answer
+
+# 아이고 간단한걸... 이렇게 돌아가기도 하는구나...
