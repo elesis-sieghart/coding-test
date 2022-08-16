868bc7675fc73eb6f0baf5d3a5184636070821c3
hyeonjeongJu <71251120+elesis-sieghart@users.noreply.github.com>
2022-08-16 23:17:18 +0900

    Update 부족한 금액 계산하기.py

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -4,6 +4,7 @@ def solution(price, money, count):
     # money - (price*1) - (price*2) ... - (price*count) 의 절댓값
     # money - (price*(n(n+1)/2))
     
+    # 돈이 남았을 경우 부족한 돈은 0원
     if (money - price*count*(count+1)/2) > 0:
         return 0
     else:
