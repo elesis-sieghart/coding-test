3372f509cdd13f5921e832e5c80382ad08b63079
hyeonjeongJu <hjju16@gmail.com>
2024-04-13 20:34:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,54 @@
-from collections import deque
+"""
+1. 아이디어
+- 2중 for, 값 1 && 방문X => DFS
+- DFS를 통해 찾은 값을 저장 후 정렬 해서 출력
 
-n = int(input())
-graph = [list(map(int, input())) for _ in range(n)]
-chk = [[False] * n for _ in range(n)]
+2. 시간복잡도
+- DFS: O(V+E)
+- V, E: N^2, 4N^2
+- V+E: 5N^2 ~= N^2 ~= 625 >> 가능
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+3. 자료구조
+- 그래프 저장: int[][]
+- 방문여부: bool[][]
+- 결과값: int[]
+"""
 
-def bfs(x, y):
-    rs = 1 
-    q = deque()
-    q.append((x, y))
-    while q:
-        ex, ey = q.popleft()
-        for k in range(4):
-            nx = ex + dx[k]
-            ny = ey + dy[k]
-            if 0<=nx<n and 0<=ny<n:
-                if graph[nx][ny] == 1 and chk[nx][ny] == False:
-                    rs += 1
-                    chk[nx][ny] = True
-                    q.append((nx, ny))
-    return rs
+import sys
+input = sys.stdin.readline
 
-cnt = 0
-vlist = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1 and chk[i][j] == False:
-            chk[i][j] = True
-            cnt += 1
-            vlist.append(bfs(i, j))
+N = int(input())
+map = [list(map(int, input().strip())) for _ in range(N)]
+chk = [[False] * N for _ in range(N)]
+result = []
+each = 0
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
 
-print(cnt)
-[print(i) for i in sorted(vlist)]
+def dfs(y, x):
+    global each # 전역변수를 안에서 사용
+    each += 1
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        if 0<=ny<N and 0<=nx<N:
+            if map[ny][nx] == 1 and chk[ny][nx] == False:
+                chk[ny][nx] = True
+                dfs(ny, nx)
+
+for j in range(N):
+    for i in range(N):
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
+            each = 0
+            dfs(j, i)
+            result.append(each)
+            # 방문 체크 표시
+            # DFS로 크기 구하기
+            # BFS: 함수 호출, 리턴값으로 크기
+            # 크기를 리스트에 넣기
+
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
